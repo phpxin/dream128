abdc6128d3b61a9c270ee565d446302de811b79e
QiCheng <lixin65535@126.com>
2016-03-17 21:26:09 +0800

    #

--- Common/config.example.php
+++ Common/config.example.php
@@ -10,20 +10,12 @@ date_default_timezone_set('Asia/ChongQing') ;
 header('content-type:text/html; charset=utf-8');
 session_start();
 
-// 如果在本地开发请在index.php同级目录建立df文件
-if(file_exists('./df')){
-	define('DEBUG_MODE', 1);
-}else{
-	define('DEBUG_MODE', 0);
-}
-
 //其他配置
 define('DS', DIRECTORY_SEPARATOR);//系统分隔符
-define('URL_MODE', 0); //url模式 0 代表普通 1代表pathinfo
 
 //缓存配置（如果不使用缓存不需要配置）
-define('CACHE_HOST','127.0.0.1');
-define('CACHE_PORT',11211);
+define('CACHE_HOST','');
+define('CACHE_PORT', 0);
 define('CACHE_STATUS', false);
 
 //数据库配置
@@ -31,7 +23,7 @@ define('DB_HOST', '127.0.0.1') ;
 define('DB_PORT', '3306') ;
 define('DB_USER', 'root') ;
 define('DB_PWD', 'lixinxin') ;
-define('DB_NAME', 'wshop') ;
+define('DB_NAME', 'dream128') ;
 define('DB_CACHE_FOLDER','./db_cache') ;
 
 
