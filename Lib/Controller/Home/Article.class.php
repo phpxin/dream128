14d05688ace2568072875a97722d12ba50342004
lixin65535@126.com <lixin65535@126.com>
2017-03-28 15:01:56 +0800

    #

--- Lib/Controller/Home/Article.class.php
+++ Lib/Controller/Home/Article.class.php
@@ -65,6 +65,45 @@ class Article extends Base
 
 		$db->add($artlog);
 	}
+	
+	
+	public function apiGetArticleList()
+	{
+		$type = getRequestInt('type', 0, 'get');
+		$page = getRequestInt('page', 1, 'get');
+
+		$limit = 25 ;
+		$plimit = 5 ;
+
+		$db = M("article");
+
+		if ($type)
+			$total = $db->where("type=".$type)->count();
+		else
+			$total = $db->count();
+
+		$pageHtml = '' ;
+		if ($total > $limit){
+			$pageObj = new \Lib\Core\Utils\Page($_SERVER['SCRIPT_NAME'] .'?', $page, $total,  $plimit, $limit) ;
+			$pageHtml = $pageObj->getPage();
+		}
+
+		$limit = (($page-1) * $limit) . ',' . $limit ;
+
+		if ($type)
+			$list = $db->field("id,title,content,addtime")->where('type='.$type)->limit($limit)->order('id desc')->select();
+		else
+			$list = $db->field("id,title,content,addtime")->limit($limit)->order('id desc')->select();
+
+		if ($list){
+			foreach ($list as $key => $val){
+				$list[$key]['content'] =  mb_substr($val['content'], 0,100, APP_CHARSET)  ;
+				$list[$key]['addtime'] = date('r', $val['addtime']);
+			}
+		}
+		
+		echo json_encode(array('code'=>200, 'data'=>$list), JSON_UNESCAPED_UNICODE);
+	}
 
 }
 ?>
